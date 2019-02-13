9b59b16b44cb79bcfd54601b1836bd44afedb64a
Crisan Razvan <crsn_razvan@yahoo.com>
2019-02-13 14:23:21 +0200

    finished js game (main) function

--- index.js
+++ index.js
@@ -1,59 +1,80 @@
 let userScore = 0;
 let compScore = 0;
-
-let userScore_span = document.getElementById('user-score');
-let compScore_span = document.getElementById('comp-score');
 let result_p = document.getElementById('result');
-let result = "";
 let rock = document.getElementById('rock');
 let paper = document.getElementById('paper');
 let scissors = document.getElementById('scissors');
 
 
 
-function compChoice(){
-    var compChoice = ["r", "p", "c"];
+function compChoice() {
+    var compChoice = ["r", "p", "s"];
     var i = Math.floor(Math.random() * 3);
     return compChoice[i];
-    
+
 }
 
-function game(userChoice){
+function game(userChoice) {
     const computerChoice = compChoice();
-    console.log(userChoice + computerChoice);
     switch (userChoice + computerChoice) {
+        //win case
         case "rs":
+            userScore++;
+            result_p.innerHTML = "Rock smashes Scissors <br> You win!";
+            break;
         case "pr":
+            userScore++;
+            result_p.innerHTML = "Paper covers Rock <br> You win!";
+            break;
         case "sp":
-        console.log("You win");
-        break;
+            userScore++;
+            result_p.innerHTML = "Scissors cuts Paper <br> You win!";
+            break;
 
+        //lose case
         case "rp":
-        case "pc":
+            userScore++;
+            result_p.innerHTML = "Rock gets covered by Paper <br> You lose!";
+            break;
+        case "ps":
+            userScore++;
+            result_p.innerHTML = "Paper gets cut by Scissors <br> You lose!";
+            break;
         case "sr":
-        console.log('you loose');
-        break;
+            userScore++;
+            result_p.innerHTML = "Scissors get smashed by Rock <br> You lose!";
+            break;
 
+        //draw case    
         case "rr":
+            compScore++;
+            result_p.innerHTML = `You both chose Rock <br>It's a draw!`
+            break;
         case "pp":
+            compScore++;
+            result_p.innerHTML = `You both chose Paper <br>It's a draw!`
+            break;
         case "ss":
-        console.log('draw');
-        break;
+            compScore++;
+            result_p.innerHTML = `You both chose Scissors <br>It's a draw!`
+            break;
     }
-    
+    document.getElementById('user-score').innerHTML = userScore;
+    document.getElementById('comp-score').innerHTML = compScore;
+
 }
 
 
-function getUserchoice (){
-    rock.addEventListener('click', function(){
+function getUserchoice() {
+    rock.addEventListener('click', function () {
         game("r");
     })
 
-    paper.addEventListener('click', function(){
+    paper.addEventListener('click', function () {
         game("p");
     })
 
-    scissors.addEventListener('click', function(){
+    scissors.addEventListener('click', function () {
         game("s");
     })
 }
